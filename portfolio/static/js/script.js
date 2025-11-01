71efd86f5d676bab471b6a12135bebab7710edf3
ARMAN HOSSAIN <Arman@ARMANs-MacBook-Pro.local>
2025-11-02 04:19:50 +0600

    Django Project Setup Done

---
+++ portfolio/static/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.getElementById("clickMeBtn");
+    btn.addEventListener("click", function() {
+        alert("Hello Arman! Your portfolio is live 🚀");
+    });
+});
